6eb6c9ecd7952cb40782a81a359cc45320d4fea3
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-10-21 14:37:17 -0700

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -28,7 +28,7 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-    bnb_url = "https://bsc-dataseed1.binance.org/"
+    bnb_url = "https://mainnet.infura.io/v3/42f8c4888a8246e8bbce242a2b8004a5"
     w3 = Web3(HTTPProvider(bnb_url))
 
     # Inject middleware
