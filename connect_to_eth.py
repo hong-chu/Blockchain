f6713803674396cb1bb2031d555cf3c89f8025e8
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-10-21 13:58:34 -0700

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -29,16 +29,16 @@ def connect_with_middleware(contract_json):
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
     # bnb_url = "https://data-seed-prebsc-1-s3.bnbchain.org:8545"
-    bnb_url = "wss://bsc-testnet-rpc.publicnode.com"
+    bnb_url = ""https://bsc-dataseed1.binance.org/""
     w3 = Web3(HTTPProvider(bnb_url))
+    assert w3.is_connected(), f"Failed to connect to BNB provider at {bnb_url}"
 
     # Inject middleware
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
-    assert w3.is_connected(), f"Failed to connect to BNB provider at {bnb_url}"
 
-	# The second section requires you to inject middleware into your w3 object and
-	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
-	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
+    # The second section requires you to inject middleware into your w3 object and
+    # create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
+    # and https://web3py.readthedocs.io/en/stable/web3.contract.html
     contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
 
     return w3, contract
