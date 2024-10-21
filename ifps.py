ff1389bac68cf3bf4e5c0ebdcb50fdbcfa3501b8
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-10-21 15:06:39 -0700

    Add files via upload

---
+++ ifps.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+
+# Define headers with your Pinata API credentials
+headers = {
+    'pinata_api_key': '242eab8dfa2af3b47a64',  # Replace with your actual API key
+    'Content-Type': 'application/json'
+}
+
+
+
+def pin_to_ipfs(data):
+	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
+	#YOUR CODE HERE
+	response = requests.post("https://api.pinata.cloud/pinning/pinJSONToIPFS", headers=headers, data=json.dumps(data))
+	cid = response.json()['IpfsHash']
+	print(f"Pinned to IPFS: {cid}")
+
+	return cid
+
+def get_from_ipfs(cid,content_type="json"):
+	assert isinstance(cid,str), f"get_from_ipfs accepts a cid in the form of a string"
+	#YOUR CODE HERE	
+	response = requests.get(f"https://gateway.pinata.cloud/ipfs/{cid}")
+	data = response.json()
+	print(f"Got from IPFS: {data}")
+
+	assert isinstance(data,dict), f"get_from_ipfs should return a dict"
+	return data
