fe8b805d5021594b56a6a5e183d08009d0e16046
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-10-21 14:38:45 -0700

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -16,6 +16,7 @@ def connect_to_eth():
 	url = "https://mainnet.infura.io/v3/784304505f3148789fea389712f68321"  # FILL THIS IN
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+	print("eth ok")
 	return w3
 
 
@@ -30,15 +31,20 @@ def connect_with_middleware(contract_json):
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
     bnb_url = "https://mainnet.infura.io/v3/42f8c4888a8246e8bbce242a2b8004a5"
     w3 = Web3(HTTPProvider(bnb_url))
+    print("w3 ok")
 
     # Inject middleware
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    print("middleware ok")
+
     assert w3.is_connected(), f"Failed to connect to BNB provider at {bnb_url}"
+    print("asset ok")
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
     contract = w3.eth.contract(address=address, abi=abi)
+    print("contract ok")
 
     return w3, contract
 
