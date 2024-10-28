581c71cc4eff5acf58db43c0b52646713a4c0c88
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-10-28 14:59:44 -0700

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -28,11 +28,12 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-    bnb_url = "https://bsc-dataseed1.binance.org/"
+    bnb_url = "https://bsc-testnet.blockpi.network/v1/rpc/public"
     w3 = Web3(HTTPProvider(bnb_url))
 
     # Inject middleware
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
     assert w3.is_connected(), f"Failed to connect to BNB provider at {bnb_url}"
 
 	# The second section requires you to inject middleware into your w3 object and
