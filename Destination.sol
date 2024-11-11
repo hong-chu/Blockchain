025b9ae01dc848a6edd39c59ecb0beb7c404d572
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-11-10 22:45:38 -0800

    Add files via upload

--- Destination.sol
+++ Destination.sol
@@ -8,48 +8,42 @@ import "./BridgeToken.sol";
 contract Destination is AccessControl {
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
-	mapping( address => address) public underlying_tokens;
-	mapping( address => address) public wrapped_tokens;
-	address[] public tokens;
+    mapping(address => address) public underlying_tokens;
+    mapping(address => address) public wrapped_tokens;
+    address[] public tokens;
 
-	event Creation( address indexed underlying_token, address indexed wrapped_token );
-	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
-	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+    event Creation(address indexed underlying_token, address indexed wrapped_token);
+    event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount);
+    event Unwrap(address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount);
 
-    constructor( address admin ) {
+    constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(CREATOR_ROLE, admin);
         _grantRole(WARDEN_ROLE, admin);
     }
 
-	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
+    function wrap(address _underlying_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
         require(underlying_tokens[_underlying_token] != address(0), "Underlying token not supported");
         require(wrapped_tokens[_underlying_token] != address(0), "Wrapped token not supported");
         require(ERC20(_underlying_token).transferFrom(_recipient, address(this), _amount), "Transfer failed");
         emit Wrap(_underlying_token, wrapped_tokens[_underlying_token], _recipient, _amount);
         ERC20(wrapped_tokens[_underlying_token]).transfer(_recipient, _amount);
-	}
+    }
 
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
+    function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
         require(wrapped_tokens[_wrapped_token] != address(0), "Wrapped token not supported");
         require(underlying_tokens[_wrapped_token] != address(0), "Underlying token not supported");
         require(ERC20(_wrapped_token).transferFrom(_recipient, address(this), _amount), "Transfer failed");
         emit Unwrap(_wrapped_token, underlying_tokens[_wrapped_token], _recipient, _amount);
         ERC20(underlying_tokens[_wrapped_token]).transfer(_recipient, _amount);
-	}
+    }
 
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-        address wrapped_token = address(new BridgeToken(name, symbol));
-        underlying_tokens[_underlying_token] = _underlying_token;
+    function createToken(address _underlying_token, string memory name, string memory symbol) public onlyRole(CREATOR_ROLE) returns(address) {
+        address wrapped_token = address(new BridgeToken(_underlying_token, name, symbol, msg.sender));
+        underlying_tokens[wrapped_token] = _underlying_token;
         wrapped_tokens[_underlying_token] = wrapped_token;
         tokens.push(_underlying_token);
         emit Creation(_underlying_token, wrapped_token);
         return wrapped_token;
-	}
-
+    }
 }
-
-
