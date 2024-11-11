2f9299866d7bbb5d137caadd9fcc3e5b76bc8a98
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-11-10 22:58:05 -0800

    Add files via upload

--- Destination.sol
+++ Destination.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./BridgeToken.sol";
 
 contract Destination is AccessControl {
-    using SafeERC20 for ERC20;
-
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
 
-    mapping(address => address) public underlying_tokens; // Maps underlying token to wrapped token
-    mapping(address => address) public wrapped_tokens;    // Maps wrapped token to underlying token
-    address[] public tokens;                              // List of all registered underlying tokens
+    // Maps between underlying tokens and their corresponding wrapped tokens
+    mapping(address => address) public underlying_tokens;
+    mapping(address => address) public wrapped_tokens;
 
+    address[] public tokens; // Tracks registered underlying tokens
+
+    // Events
     event Creation(address indexed underlying_token, address indexed wrapped_token);
     event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed recipient, uint256 amount);
     event Unwrap(address indexed wrapped_token, address indexed underlying_token, address indexed recipient, uint256 amount);
@@ -28,20 +28,20 @@ contract Destination is AccessControl {
 
     /**
      * @dev Creates a wrapped token for the given underlying asset.
-     * Can only be called by an address with CREATOR_ROLE.
+     * Only addresses with the CREATOR_ROLE can call this function.
      */
-    function createToken(address _underlying_token, string memory name, string memory symbol) 
-        public 
-        onlyRole(CREATOR_ROLE) 
-        returns (address) 
+    function createToken(address _underlying_token, string memory name, string memory symbol)
+        public
+        onlyRole(CREATOR_ROLE)
+        returns (address)
     {
         require(_underlying_token != address(0), "Invalid underlying token address");
         require(underlying_tokens[_underlying_token] == address(0), "Token already registered");
 
-        // Deploy a new BridgeToken
+        // Deploy the new BridgeToken
         BridgeToken wrapped_token = new BridgeToken(_underlying_token, name, symbol, msg.sender);
 
-        // Register the token mappings
+        // Map the underlying token to the new wrapped token
         underlying_tokens[_underlying_token] = address(wrapped_token);
         wrapped_tokens[address(wrapped_token)] = _underlying_token;
         tokens.push(_underlying_token);
@@ -52,7 +52,7 @@ contract Destination is AccessControl {
 
     /**
      * @dev Wraps the specified amount of an underlying token into its corresponding wrapped token.
-     * Can only be called by an address with WARDEN_ROLE.
+     * Only addresses with the WARDEN_ROLE can call this function.
      */
     function wrap(address _underlying_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
         require(_underlying_token != address(0), "Invalid underlying token address");
@@ -70,7 +70,8 @@ contract Destination is AccessControl {
     }
 
     /**
-     * @dev Unwraps the specified amount of a wrapped token, burning it and preparing the underlying token for release.
+     * @dev Unwraps the specified amount of a wrapped token.
+     * Burns the wrapped tokens and prepares the underlying tokens for release.
      */
     function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
         require(_wrapped_token != address(0), "Invalid wrapped token address");
@@ -81,14 +82,14 @@ contract Destination is AccessControl {
         address underlying_token = wrapped_tokens[_wrapped_token];
         require(underlying_token != address(0), "Underlying token not found");
 
-        // Burn the wrapped tokens
+        // Burn the wrapped tokens from the sender
         BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
 
         emit Unwrap(_wrapped_token, underlying_token, _recipient, _amount);
     }
 
     /**
-     * @dev Returns the list of registered tokens.
+     * @dev Returns the list of registered underlying tokens.
      */
     function getTokens() public view returns (address[] memory) {
         return tokens;
