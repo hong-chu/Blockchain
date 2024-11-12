646e21154e002b71af14db55480bd6c70981a888
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-11-11 19:04:11 -0800

    Add files via upload

--- Destination.sol
+++ Destination.sol
@@ -8,68 +8,55 @@ import "./BridgeToken.sol";
 contract Destination is AccessControl {
     bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
     bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
-	mapping( address => address) public underlying_tokens;
-	mapping( address => address) public wrapped_tokens;
-	address[] public tokens;
+    mapping(address => address) public underlying_tokens;
+    mapping(address => address) public wrapped_tokens;
+    address[] public tokens;
 
-	event Creation( address indexed underlying_token, address indexed wrapped_token );
-	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
-	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+    event Creation(address indexed underlying_token, address indexed wrapped_token);
+    event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount);
+    event Unwrap(address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount);
 
-    constructor( address admin ) {
+    constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(CREATOR_ROLE, admin);
         _grantRole(WARDEN_ROLE, admin);
     }
 
-	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
-		//YOUR CODE HERE
-    	// Get the wrapped token address
+    function wrap(address _underlying_token, address _recipient, uint256 _amount) public onlyRole(WARDEN_ROLE) {
         address wrapped_token = underlying_tokens[_underlying_token];
-    
-        // Ensure sufficient approval
-        IERC20(_underlying_token).approve(address(this), _amount);
-    
-        // Transfer the underlying token from the sender to this contract
-        IERC20(_underlying_token).transferFrom(msg.sender, address(this), _amount);
-    
-        // Mint the wrapped token to the recipient
+        require(wrapped_token != address(0), "Wrapped token does not exist");
+
+        // Mint the wrapped tokens to the recipient
         BridgeToken(wrapped_token).mint(_recipient, _amount);
-    
+
         emit Wrap(_underlying_token, wrapped_token, _recipient, _amount);
-    
-	}
+    }
 
-	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
-		//YOUR CODE HERE
-        // Get the underlying token address
+    function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
+        require(wrapped_tokens[_wrapped_token] != address(0), "Wrapped token does not exist");
+
+        // Burn the wrapped tokens from the sender
+        BridgeToken token = BridgeToken(_wrapped_token);
+        token.burnFrom(msg.sender, _amount);
+
         address underlying_token = wrapped_tokens[_wrapped_token];
-    
-        // Burn the wrapped token from the sender
-        BridgeToken(_wrapped_token).burnFrom(msg.sender, _amount);
-    
-        // Transfer the underlying token to the recipient
-        IERC20(underlying_token).transfer(_recipient, _amount);
-    
+
         emit Unwrap(underlying_token, _wrapped_token, msg.sender, _recipient, _amount);
+    }
 
-	}
+    function createToken(address _underlying_token, string memory name, string memory symbol) public onlyRole(CREATOR_ROLE) returns (address) {
+        require(underlying_tokens[_underlying_token] == address(0), "Token already exists");
 
-	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
-		//YOUR CODE HERE
-        // Create a new BridgeToken contract
-        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, msg.sender);
-    
-        // Store the mapping between underlying and wrapped tokens
+        // Deploy a new BridgeToken contract
+        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+
+        // Map the underlying token to the new wrapped token
         underlying_tokens[_underlying_token] = address(newToken);
         wrapped_tokens[address(newToken)] = _underlying_token;
         tokens.push(address(newToken));
-    
+
         emit Creation(_underlying_token, address(newToken));
-    
+
         return address(newToken);
-	}
-
+    }
 }
-
-
