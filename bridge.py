9879cc472ef674de90fbe0b911f1e134c096c891
hong-chu <105093753+hong-chu@users.noreply.github.com>
2024-12-06 11:00:08 -0800

    Add files via upload

---
+++ bridge.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 10:58:35 2024
+
+@author: hongchu
+"""
+
+from web3 import Web3
+from web3.contract import Contract
+from web3.providers.rpc import HTTPProvider
+from web3.middleware import geth_poa_middleware #Necessary for POA chains
+import json
+import sys
+from pathlib import Path
+
+source_chain = 'avax'
+destination_chain = 'bsc'
+contract_info = "contract_info.json"
+
+def connectTo(chain):
+    if chain == 'avax':
+        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
+
+    if chain == 'bsc':
+        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
+
+    if chain in ['avax','bsc']:
+        w3 = Web3(Web3.HTTPProvider(api_url))
+        # inject the poa compatibility middleware to the innermost layer
+        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    return w3
+
+def getContractInfo(chain):
+    """
+        Load the contract_info file into a dictinary
+        This function is used by the autograder and will likely be useful to you
+    """
+    p = Path(__file__).with_name(contract_info)
+    try:
+        with p.open('r')  as f:
+            contracts = json.load(f)
+    except Exception as e:
+        print( "Failed to read contract info" )
+        print( "Please contact your instructor" )
+        print( e )
+        sys.exit(1)
+
+    return contracts[chain]
+
+
+
+def scanBlocks(chain):
+    """
+        chain - (string) should be either "source" or "destination"
+        Scan the last 5 blocks of the source and destination chains
+        Look for 'Deposit' events on the source chain and 'Unwrap' events on the destination chain
+        When Deposit events are found on the source chain, call the 'wrap' function the destination chain
+        When Unwrap events are found on the destination chain, call the 'withdraw' function on the source chain
+    """
+
+    if chain not in ['source','destination']:
+        print( f"Invalid chain: {chain}" )
+        return
+
+    w3 = connectTo(chain)
+    contract = getContractInfo(chain)
+    contract_address = contract['address']
+    contract_abi = contract['abi']
+    contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+    if chain == 'source':
+        event_filter = contract.events.Deposit.create_filter(fromBlock=w3.eth.block_number-5,toBlock=w3.eth.block_number)
+        events = event_filter.get_all_entries()
+        for event in events:
+            print(event)
+            print(event['args'])
+            print(event['args']['amount'])
+            print(event['args']['recipient'])
+            print(event['args']['token'])
+
+    else:
+        event_filter = contract.events.Unwrap.create_filter(fromBlock=w3.eth.block_number-5,toBlock=w3.eth.block_number)
+        events = event_filter.get_all_entries()
+        for event in events:
+            print(event)
+            print(event['args'])
+            print(event['args']['amount'])
+            print(event['args']['recipient'])
+            print(event['args']['token'])
+
+                
+
+
+
